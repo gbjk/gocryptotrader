f1f13378156bb5bff856dda5488f278102c61a97
Gareth Kirwan <gbjkirwan@gmail.com>
2025-06-14 15:35:17 +0700

    fixup! fixup! Accounts: Bitstamp update

--- internal/testing/exchange/exchange.go
+++ internal/testing/exchange/exchange.go
@@ -17,8 +17,8 @@ import (
 	"github.com/thrasher-corp/gocryptotrader/config"
 	"github.com/thrasher-corp/gocryptotrader/currency"
 	"github.com/thrasher-corp/gocryptotrader/dispatch"
-	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
 	"github.com/thrasher-corp/gocryptotrader/exchange/accounts"
+	exchange "github.com/thrasher-corp/gocryptotrader/exchanges"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/mock"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/sharedtestvalues"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/subscription"
@@ -51,7 +51,7 @@ func Setup(e exchange.IBotExchange) error {
 		return fmt.Errorf("Setup() error: %w", err)
 	}
 
-	b.Accounts = accounts.MustNewAccounts(eName, dispatch.GetNewMux(nil))
+	b.Accounts = accounts.MustNewAccounts(b, dispatch.GetNewMux(nil))
 
 	return err
 }
