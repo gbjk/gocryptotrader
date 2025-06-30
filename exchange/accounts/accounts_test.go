ffd77d8dc1e3c3b65b8aa1b59e53916c7d1853b4
Gareth Kirwan <gbjkirwan@gmail.com>
2025-06-30 15:44:35 +0700

    fixup! Accounts: TestCurrencyBalancesPrivate coverage

--- exchange/accounts/accounts_test.go
+++ exchange/accounts/accounts_test.go
@@ -59,11 +59,33 @@ func TestCurrencyBalancesPrivate(t *testing.T) {
 	a := &Accounts{subAccounts: make(credSubAccounts)}
 	c := &Credentials{Key: "a"}
 	b := a.currencyBalances(c, "", asset.Spot)
-	r := a.subAccounts[*c][key.SubAccountAsset{Asset: asset.Spot}]
-	assert.Equal(t, reflect.ValueOf(b).UnsafePointer(), reflect.ValueOf(r).UnsafePointer(), "should return the same map")
+	r1 := a.subAccounts[*c]
+	assert.Equal(t,
+		reflect.ValueOf(b).UnsafePointer(),
+		reflect.ValueOf(r1[key.SubAccountAsset{Asset: asset.Spot}]).UnsafePointer(),
+		"should make and return the same map")
+	assert.Equal(t,
+		reflect.ValueOf(b).UnsafePointer(),
+		reflect.ValueOf(a.currencyBalances(c, "", asset.Spot)).UnsafePointer(),
+		"should return the same map on subsequent calls")
 	b = a.currencyBalances(c, "", asset.Futures)
-	r = a.subAccounts[*c][key.SubAccountAsset{Asset: asset.Futures}]
-	assert.Equal(t, reflect.ValueOf(b).UnsafePointer(), reflect.ValueOf(r).UnsafePointer(), "should return the same map")
+	assert.Equal(t,
+		reflect.ValueOf(r1).UnsafePointer(),
+		reflect.ValueOf(a.subAccounts[*c]).UnsafePointer(),
+		"should not make a new cred key")
+	assert.Equal(t,
+		reflect.ValueOf(b).UnsafePointer(),
+		reflect.ValueOf(r1[key.SubAccountAsset{Asset: asset.Futures}]).UnsafePointer(),
+		"should make and return the same map")
+}
+
+// TestCurrencyBalancesBalance exercises currencyBalances.balance
+func TestCurrencyBalancesBalance(t *testing.T) {
+	t.Parallel()
+
+	c := currencyBalances{}
+	_ = c
+	// c.balance(
 }
 
 /*
