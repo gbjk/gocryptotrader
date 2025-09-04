5570536890948234952996204182ae3582006c19
Gareth Kirwan <gbjkirwan@gmail.com>
2025-09-04 19:55:37 +0700

    Websocket: Filter subs by connection

--- exchange/websocket/connection.go
+++ exchange/websocket/connection.go
@@ -20,6 +20,7 @@ import (
 
 	gws "github.com/gorilla/websocket"
 	"github.com/thrasher-corp/gocryptotrader/encoding/json"
+	"github.com/thrasher-corp/gocryptotrader/exchanges/asset"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/request"
 	"github.com/thrasher-corp/gocryptotrader/exchanges/subscription"
 	"github.com/thrasher-corp/gocryptotrader/log"
@@ -101,7 +102,7 @@ type ConnectionSetup struct {
 	Authenticate func(ctx context.Context, conn Connection) error
 	// MessageFilter defines the criteria used to match messages to a specific connection.
 	// The filter enables precise routing and handling of messages for distinct connection contexts.
-	MessageFilter any
+	MessageFilter MessageFilter
 }
 
 // Inspector is used to verify messages via SendMessageReturnResponsesWithInspection
@@ -512,4 +513,16 @@ func (c *connection) RequireMatchWithData(signature any, incoming []byte) error 
 // IncomingWithData routes incoming data using the connection specific match system to the correct handler
 func (c *connection) IncomingWithData(signature any, data []byte) bool {
 	return c.Match.IncomingWithData(signature, data)
+}
+
+type MessageFilter interface {
+	MatchesSub(*subscription.Subscription) bool
+}
+
+type AssetFilter asset.Item
+
+var _ MessageFilter = AssetFilter(asset.Spot)
+
+func (a AssetFilter) MatchesSub(s *subscription.Subscription) bool {
+	return s.Asset == asset.Item(a)
 }
--- exchanges/bybit/bybit_wrapper.go
+++ exchanges/bybit/bybit_wrapper.go
@@ -307,7 +307,7 @@ func (e *Exchange) Setup(exch *config.Exchange) error {
 			return e.wsHandleData(conn, asset.USDTMarginedFutures, resp)
 		},
 		RequestIDGenerator: e.messageIDSeq.IncrementAndGet,
-		MessageFilter:      asset.USDTMarginedFutures, // Unused but it allows us to differentiate between the two linear futures types.
+		MessageFilter:      websocket.AssetFilter(asset.USDTMarginedFutures), // Unused but it allows us to differentiate between the two linear futures types.
 	}); err != nil {
 		return err
 	}
@@ -336,7 +336,7 @@ func (e *Exchange) Setup(exch *config.Exchange) error {
 			return e.wsHandleData(conn, asset.USDCMarginedFutures, resp)
 		},
 		RequestIDGenerator: e.messageIDSeq.IncrementAndGet,
-		MessageFilter:      asset.USDCMarginedFutures, // Unused but it allows us to differentiate between the two linear futures types.
+		MessageFilter:      websocket.AssetFilter(asset.USDCMarginedFutures), // Unused but it allows us to differentiate between the two linear futures types.
 	}); err != nil {
 		return err
 	}
