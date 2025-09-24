7c2f5e3be815b264da450926c55de9534b6526c4
Gareth Kirwan <gbjkirwan@gmail.com>
2025-09-24 14:55:25 +0700

    fixup! Accounts: Move singleton accounts service to exchange Accounts

--- exchange/accounts/store_test.go
+++ exchange/accounts/store_test.go
@@ -20,8 +20,8 @@ func TestGetStore(t *testing.T) {
 	// Initialize global in case of -count=N+; No other tests should be relying on it
 	global.Store(nil)
 	s := GetStore()
-	require.NotNil(t, s)
-	require.Same(t, global.Load(), s, "GetStore must initialize the store")
+	require.NotNil(t, s, "GetStore must return a Store")
+	require.Same(t, global.Load(), s, "GetStore must set the global store")
 	require.Same(t, s, GetStore(), "GetStore must return the global store on second call")
 }
 
